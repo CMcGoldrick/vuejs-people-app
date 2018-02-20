a3fac3209327defbe9977cfeb7f6062b558c0fa8
JackMarx <seriousfools@gmail.com>
2018-02-19 19:34:05 -0600

    create

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,15 @@
+class PeopleController < ApplicationController
+  def index
+    @people = Person.all
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @person = Person.new(
+                         name: params[:name],
+                         bio: params[:bio]
+                        )
+    @person.save
+    render 'show.json.jbuilder'
+  end
+end
